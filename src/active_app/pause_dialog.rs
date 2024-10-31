edbb5e7a851d2a1b41b7d0e821b473142934ad56
Trent Shailer <trent.shailer@gmail.com>
2024-10-31 14:45:25 +1300

    added comments about the win32 dialog

--- src/active_app/pause_dialog.rs
+++ src/active_app/pause_dialog.rs
@@ -10,6 +10,8 @@ use windows::Win32::{
     },
 };
 
+// Following are some constants that are the utf16 null-terminated strings used in the pause dialog.
+
 const DIALOG_TITLE: [u16; 22] = [
     0x0050, 0x0061, 0x0075, 0x0073, 0x0065, 0x0020, 0x0042, 0x0072, 0x0065, 0x0061, 0x006b, 0x0020,
     0x0052, 0x0065, 0x006d, 0x0069, 0x006e, 0x0064, 0x0065, 0x0072, 0x0073, 0x0000,
@@ -47,8 +49,35 @@ pub const OK_ID_ISIZE: isize = IDH_OK as isize;
 
 pub const INPUT_ID: u16 = 125;
 
+/// A win32 dialog is defined by a structure called a [`DLGTEMPLATE`](https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-dlgtemplate).
+/// The windows crate contains a definition for this structure as in the `dialog_template` field,
+/// however, creating a dialog requires more than just the `DLGTEMPLATE`.
+///
+/// The function to create a dialog requres the `DLGTEMPLATE` structure to be followed immediately in
+/// memory by a variable length array made up of 16-bit elements that are aligned in memory on WORD
+/// boundaries. If the first element in this array is `0x0000`, then the dialog has no menu and the
+/// array has no other elements. I don't expect to need a menu so this is a u16 that should always
+/// be `0x0000`.
+///
+/// Then the menu should be followed by another variable length array, the same as above, but this
+/// defines the class for the dialog. If the first element is `0x0000` then the default dialog class
+/// is used. I only need the default dialog class so this is a u16 that should always be `0x0000`.
+///
+/// This is followed by another variable length array, the same as above, but this defines the title
+/// for the dialog. This should be a utf16 encoded null-terminated string. However, this must be
+/// directly after the class in memory, so a pointer to a string on the heap is not acceptable.
+/// Therefore an array with a generic const length is used. If the first item in the array is `0x0000`
+/// then the dialog has no title.
+///
+/// Then ONLY IF the `DLGTEMPLATE` has the `DS_SETFONT` flag in it's style, the title should be followed
+/// by a 16-bit int for the size of the font which is in turn followed by a variable length array
+/// the same as the title for the name of the font. Because I expect to always use the `DS_SETFONT`
+/// flag, these are present in this structure.
+///
+/// Then based on the `cdit` field of the `DLGTEMPLATE`, the will expect the font to be followed
+/// by `cdit` number of `DialogItemTemplates` in memory for the fields that make up the dialog.
 #[repr(C, align(4))]
-pub struct DialogTemplate<const L: usize, const F: usize> {
+pub struct DialogTemplate<const TITLE_LENGTH: usize, const FONT_LENGTH: usize> {
     pub dialog_template: DLGTEMPLATE,
 
     /// Variable length array, 16-bit elements, aligned on word boundaries,
@@ -61,23 +90,26 @@ pub struct DialogTemplate<const L: usize, const F: usize> {
 
     /// Variable length array, 16-bit elements, aligned on word boundaries.
     /// If the first element of this array is 0x0000, the dialog box has no title and the array has no other elements.
-    pub title: [u16; L],
+    pub title: [u16; TITLE_LENGTH],
 
     /// Font size
     pub font_size: u16,
 
     /// Font
-    pub font: [u16; F],
+    pub font: [u16; FONT_LENGTH],
 }
 
+/// A `DialogItemTemplate` is similar to a `DialogTemplate` as it is made up of a `DLGITEMTEMPLATE`
+/// that is then followed by some dynamic length arrays.
 #[repr(C, align(4))]
-pub struct DialogItemTemplate<const L: usize> {
+pub struct DialogItemTemplate<const TITLE_LENGTH: usize> {
     pub template: DLGITEMTEMPLATE,
     pub class: [u16; 2],
-    pub title: [u16; L],
+    pub title: [u16; TITLE_LENGTH],
     pub creation_data: u16,
 }
 
+/// The pause dialog template is a dialog template followed by the items that make it up.
 #[repr(C, align(4))]
 pub struct PauseDialogTemplate {
     pub dialog: DialogTemplate<DIALOG_TITLE_LENGTH, FONT_LENGTH>,

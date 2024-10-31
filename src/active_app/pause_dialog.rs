caa8d8bf1d3378ebe74d9661c117bfa45075783d
Trent Shailer <trent.shailer@gmail.com>
2024-10-31 14:58:20 +1300

    more details on the dialog

--- src/active_app/pause_dialog.rs
+++ src/active_app/pause_dialog.rs
@@ -49,7 +49,8 @@ pub const OK_ID_ISIZE: isize = IDH_OK as isize;
 
 pub const INPUT_ID: u16 = 125;
 
-/// A win32 dialog is defined by a structure called a [`DLGTEMPLATE`](https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-dlgtemplate).
+/// A win32 dialog is defined in a single continous block of memory.
+/// It starts structure called a [`DLGTEMPLATE`](https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-dlgtemplate).
 /// The windows crate contains a definition for this structure as in the `dialog_template` field,
 /// however, creating a dialog requires more than just the `DLGTEMPLATE`.
 ///
@@ -68,6 +69,13 @@ pub const INPUT_ID: u16 = 125;
 /// directly after the class in memory, so a pointer to a string on the heap is not acceptable.
 /// Therefore an array with a generic const length is used. If the first item in the array is `0x0000`
 /// then the dialog has no title.
+/// For a simplified look as to why this is complicated, a String variable is essentially a stack allocated
+/// pointer, length, and capacity variables where the actual contents of the string are on the heap.
+/// This is a problem because when the windows API is trying to create the dialog it is just reading
+/// the bytes of memory that follow the previous bytes which if a string were used are just the pointer,
+/// length, and capacity. Therefore, the title must be the contents of the title on the stack with no
+/// other information. The way I acheive this is to use an array, however, array lengths must be known
+/// at compile time, and therefore need to be defined as a generic parameter to the structure.
 ///
 /// Then ONLY IF the `DLGTEMPLATE` has the `DS_SETFONT` flag in it's style, the title should be followed
 /// by a 16-bit int for the size of the font which is in turn followed by a variable length array
@@ -76,7 +84,7 @@ pub const INPUT_ID: u16 = 125;
 ///
 /// Then based on the `cdit` field of the `DLGTEMPLATE`, the will expect the font to be followed
 /// by `cdit` number of `DialogItemTemplates` in memory for the fields that make up the dialog.
-#[repr(C, align(4))]
+#[repr(C, align(2))]
 pub struct DialogTemplate<const TITLE_LENGTH: usize, const FONT_LENGTH: usize> {
     pub dialog_template: DLGTEMPLATE,
 
